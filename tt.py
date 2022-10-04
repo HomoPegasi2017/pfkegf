b5677c426d09071cae7c1099a3ed4c702976ba16
HomoPegasi2017 <114360806+HomoPegasi2017@users.noreply.github.com>
2022-10-04 14:29:12 +0300

    Update tt.py

--- tt.py
+++ tt.py
@@ -1,13 +1,16 @@
-a = float(input('a = '))
-b = float(input('b = '))
-c = float(input('c = '))
-d = b**2 - 4 * a * c
-if d > 0:
-    x1 = (-b + (d**0.5) / (2 * a))
-    x2 = (-b - (d**0.5) / (2 * a))
-    print(x1,x2)
-elif d == 0:
-    x = -b / (2 * a)
-    print(x)
-if d < 0:
-    print("no roots")
+print('ax^2+bx+c')
+a=float(input('a = '))
+b=float(input('b = '))
+c=float(input('c = '))
+d=b**2-4*a*c
+print('discriminant = ', d)
+if d>0:
+	x1=(-b+d**0.5)/(2*a)
+	x2=(-b-d**0.5)/(2*a)
+	print('x1 = ', str(x1))
+	print('x2 = ', str(x2))
+if d==0:
+	x=-b/(2*a)
+	print('x = ', str(x))
+else:
+	print('korney net')
